97cfd9458fd11ad6080a0e225b3c5ced231d39a6
Cinition <cornee.n@gmail.com>
2022-11-24 21:40:11 +0100

    Added namespace to string

--- Src/Misc/String.h
+++ Src/Misc/String.h
@@ -3,27 +3,31 @@
 #include <cstdarg>
 #include <string>
 
-class String
+namespace Singularity
 {
-
-public:
-
-    String() = default;
-    String(std::string _string) : m_string(_string) {};
-    String(const char* _format, ...) { va_list args; va_start(args, _format); Format(_format, args); va_end(args); };
-
-    const char* ToChar() { return m_string.c_str(); }
-
-private:
-
-    void Format( const char* _format, va_list _args )
+    class String
     {
-        char buffer[256];
-        size_t string_size = vsnprintf(buffer, 256, _format, _args);
-        m_string = buffer;
-        m_string.resize(string_size);
-    }
 
-    std::string m_string = "";
+    public:
 
-};
+        String() = default;
+        String(std::string _string) : m_string(_string) {};
+        String(const char* _format, ...) { va_list args; va_start(args, _format); Format(_format, args); va_end(args); };
+
+        const char* ToChar() { return m_string.c_str(); }
+
+    private:
+
+        void Format(const char* _format, va_list _args)
+        {
+            char buffer[256];
+            size_t string_size = vsnprintf(buffer, 256, _format, _args);
+            m_string = buffer;
+            m_string.resize(string_size);
+        }
+
+        std::string m_string = "";
+
+    };
+
+}
